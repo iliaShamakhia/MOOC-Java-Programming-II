1844478c034b6eaff57e37087436f566276684ec
iliaShamakhia <shamakhiailia@gmail.com>
2022-04-08 13:57:29 +0400

    exercises 1-5

---
+++ Part10/05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintingUserInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList();
+        while(true){
+            String input = scanner.nextLine();
+            if(input.isEmpty()){
+                break;
+            }
+            words.add(input);
+        }
+        words.stream().forEach(el -> System.out.println(el));
+    }
+}
