e34f33db2c5944f4eada7387f81cce04c411776a
iliaShamakhia <shamakhiailia@gmail.com>
2022-04-11 14:50:18 +0400

    exercises 1-5

---
+++ Part11/05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    private String name;
+    
+    public void play(){
+        System.out.println("pleyer is playing");
+    }
+    public void printName(){
+        System.out.println(this.name);
+    }
+}
