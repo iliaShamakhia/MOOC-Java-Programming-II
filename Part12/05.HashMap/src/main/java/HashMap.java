d2724dbcc2877e3e0cb67bd29a505a7b6095cb71
iliaShamakhia <shamakhiailia@gmail.com>
2022-04-14 14:12:38 +0400

    exercises 1-5

---
+++ Part12/05.HashMap/src/main/java/HashMap.java
@@ -0,0 +1,74 @@
+
+import java.util.ArrayList;
+
+public class HashMap<K, V> {
+
+    private ArrayList<Pair<K, V>>[] values;
+    
+    public HashMap() {
+        this.values = new ArrayList[32];
+    }
+    
+    public V get(K key) {
+        int hashValue = Math.abs(key.hashCode() % this.values.length);
+        if (this.values[hashValue] == null) {
+            return null;
+        }
+
+        ArrayList<Pair<K, V>> valuesAtIndex = this.values[hashValue];
+
+        for (int i = 0; i < valuesAtIndex.size(); i++) {
+            if (valuesAtIndex.get(i).getKey().equals(key)) {
+                return valuesAtIndex.get(i).getValue();
+            }
+        }
+
+        return null;
+    }
+    
+    public void add(K key, V value) {
+        ArrayList<Pair<K, V>> valuesAtIndex = getListBasedOnKey(key);
+        int index = getIndexOfKey(valuesAtIndex, key);
+
+        if (index < 0) {
+            valuesAtIndex.add(new Pair<>(key, value));
+        } else {
+            valuesAtIndex.get(index).setValue(value);
+        }
+    }
+    
+    private ArrayList<Pair<K, V>> getListBasedOnKey(K key) {
+        int hashValue = Math.abs(key.hashCode() % this.values.length);
+        if (values[hashValue] == null) {
+            values[hashValue] = new ArrayList<Pair<K,V>>();
+        }
+
+        return values[hashValue];
+    }
+    
+    private int getIndexOfKey(ArrayList<Pair<K, V>> myList, K key) {
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i).getKey().equals(key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+
+    public V remove(K key) {
+        ArrayList<Pair<K, V>> valuesAtIndex = getListBasedOnKey(key);
+        if (valuesAtIndex.size() == 0) {
+            return null;
+        }
+
+        int index = getIndexOfKey(valuesAtIndex, key);
+        if (index < 0) {
+            return null;
+        }
+
+        Pair<K, V> pair = valuesAtIndex.get(index);
+        valuesAtIndex.remove(pair);
+        return pair.getValue();
+    }
+}
